2ba5b4933c3dc597e40ff3db4c6158a12d8ee200
Veronika <veronikaronya99@gmail.com>
2020-11-24 19:48:34 +0300

    OOP

---
+++ OOP/OOP/Piano.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Piano : Musical_instrument
+    {
+        public bool piano_pedals;//педали - истинно для фортепиано
+        public Piano(string name, string brand, int price, bool mobility, bool piano_pedals) : base(name, brand, price, mobility)
+        {
+            this.piano_pedals = piano_pedals;
+        }
+
+        public override void Sound()//реализация абстрактного метода
+        {
+            Console.WriteLine("Фортепиано звучит");
+        }
+    }
+}
