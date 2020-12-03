f019b10e94dbc7d4ea1a5018859ef4bf011bd3b2
Veronika <veronikaronya99@gmail.com>
2020-12-03 08:08:30 +0300

    Coll

--- OOP/Classical.cs
+++ OOP/Classical.cs
@@ -15,5 +15,11 @@ namespace OOP
         {
             Console.WriteLine("Источник звука в классической гитаре - розетка");
         }
+        public override string ToString()
+        {
+            return String.Format("Классическая гитара: название - {0}, бренд - {1}, цена = {2}, мобильность = {3}, количество струн = {4}, наличие резонатора = {5}", this.name, this.brand, this.price, this.mobility, this.number_of_strings, this.resonator_hole);
+        }
+
+      
     }
 }
