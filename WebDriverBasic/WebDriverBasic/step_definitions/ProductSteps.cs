c133f18caab3a3f3eccab4d6071ac1230dcc43e4
Veronika <veronikaronya99@gmail.com>
2020-12-24 14:52:31 +0300

    .

--- WebDriverBasic/WebDriverBasic/step_definitions/ProductSteps.cs
+++ WebDriverBasic/WebDriverBasic/step_definitions/ProductSteps.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using TechTalk.SpecFlow;
@@ -8,5 +10,21 @@ namespace WebDriverFramework.step_definitions
     [Binding]
     class ProductSteps
     {
+        public int index;
+        private IWebDriver driver;
+        [Given(@"I open ""(.+)"" url")]
+        public void GivenIOpenMainPage(string url)
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+        }
+
+        [AfterScenario]
+        public void CloseDriver()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
     }
 }
